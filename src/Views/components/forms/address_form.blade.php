c5936aa5e1211c38ad0dd7dcd0d9399913f30583
rafazingano <rafazingano@gmail.com>
2019-12-27 09:18:54 -0300

    Update all

--- src/Views/components/forms/address_form.blade.php
+++ src/Views/components/forms/address_form.blade.php
@@ -1,53 +1,53 @@
-<div class="col-lg-6 col-xl-6">
+<div class="col-12">
     <div class="form-group ">
         <label class="form-label">{{ __('location::address.cep') }}</label>
         {!! Form::text('sync[address][postal_code]', NULL, ['data-mask' => config('erp.address.postal_code_mask'), 'id' => 'postal_code', 'data-autocomplete' => 'true', 'class' => 'form-control', 'placeholder' => 'CEP']) !!}
     </div>
 </div>
 
-<div class="col-lg-6 col-xl-6">
+<div class="col-12">
     <div class="form-group ">
         <label class="form-label">{{ __('location::address.country') }}</label>
         {{ Form::select('sync[address][country_id]', [], NULL, ['id' => 'country', 'data-default' => config('erp.address.default.country'), 'class' => 'form-control']) }}
     </div>
 </div>
 
-<div class="col-lg-6 col-xl-6">
+<div class="col-12">
     <div class="form-group ">
         <label class="form-label">{{ __('location::address.state') }}</label>
         {{ Form::select('sync[address][state_id]', [], NULL, ['id' => 'state', 'data-default' => config('erp.address.default.state'), 'class' => 'form-control']) }}
     </div>
 </div>
 
-<div class="col-lg-6 col-xl-6">
+<div class="col-12">
     <div class="form-group ">
         <label class=form-label">{{ __('location::address.city') }}</label>
         {{ Form::select('sync[address][city_id]', [], NULL, ['id' => 'city', 'class' => 'form-control']) }}
     </div>
 </div>
 
-<div class="col-lg-6 col-xl-6">
+<div class="col-12">
     <div class="form-group ">
         <label class="form-label">{{ __('location::address.neighborhood') }}</label>
         {!! Form::text('sync[address][neighborhood]', NULL, ['id' => 'neighborhood', 'class' => 'form-control', 'placeholder' => 'Bairro']) !!}
     </div>
 </div>
 
-<div class="col-lg-6 col-xl-6">
+<div class="col-12">
     <div class="form-group ">
         <label class="form-label">{{ __('location::address.street') }}</label>
         {!! Form::text('sync[address][street]', NULL, ['id' => 'street', 'class' => 'form-control', 'placeholder' => 'Rua']) !!}
     </div>
 </div>
 
-<div class="col-lg-6 col-xl-6">
+<div class="col-12">
     <div class="form-group ">
         <label class="form-label">{{ __('location::address.number') }}</label>
         {!! Form::text('sync[address][number]', NULL, ['id' => 'number', 'class' => 'form-control', 'placeholder' => 'Numero']) !!}
     </div>
 </div>
 
-<div class="col-lg-6 col-xl-6">
+<div class="col-12">
     <div class="form-group ">
         <label class="form-label">{{ __('location::address.complement') }}</label>
         {!! Form::text('sync[address][complement]', NULL, ['id' => 'complement', 'class' => 'form-control', 'placeholder' => 'Complemento']) !!}
